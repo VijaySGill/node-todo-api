41d5336e50f5c4d6685bcead58bca2b3cbaf01f4
Vajinder Gill <v.gill@se15.qmul.ac.uk>
2018-07-29 15:21:23 +0100

    Added DELETE /users/me/token test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -372,3 +372,35 @@ describe('POST /users/login', function()
         });
   });
 });
+
+describe('DELETE /users/me/token', function()
+{
+  it('should remove auth token on logout', function(done)
+  {
+    var token = users[0].tokens[0].token
+
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', token)
+      .expect(200)
+      .end(function(error, message)
+      {
+        if(error)
+        {
+          return done(error)
+        }
+
+        else
+        {
+          User.findById(users[0]._id).then(function(user)
+          {
+            expect(user.tokens.length).toBe(0);
+            done();
+          }).catch(function(error)
+          {
+            done(error);
+          });
+        }
+      });
+  });
+});
